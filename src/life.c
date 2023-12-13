6049a1651c7647451c3fd585ff4bdb7d1403bf78
Cart Benjamin <51527949+LeBenyeh@users.noreply.github.com>
2023-12-13 18:03:46 +0100

    Update life.c

--- src/life.c
+++ src/life.c
@@ -4,6 +4,63 @@
 #include <unistd.h>
 #include "inout.h"
 #define Time 1
+
+int count(int lig, int col, bool univ[HEIGHT][WIDTH])
+{
+  int c=0, i, j;
+  for(i=-1; i<2;i++)
+  {
+    for(j=-1; j<2;j++)
+    {
+      if(lig+i >= 0 && lig+i <= HEIGHT)
+      {
+        if(col+j >= 0 && col+j <= WIDTH)
+        {
+          if(lig+i != lig || col+j != col)
+          {
+            if(univ[lig+i][col+j] == 1)
+            {
+              c++;
+            }
+          }
+
+        }
+      }
+    }
+  }
+  return c;
+}
+
+
+void evolve(bool univ[HEIGHT][WIDTH])
+{
+  bool univ2[HEIGHT][WIDTH]={0};
+  int neigh;
+  for (int i=0; i<HEIGHT; i++)
+  {
+    for (int j=0; j<WIDTH; j++)
+    {
+      neigh = count(i,j,univ);
+      if(univ[i][j] == 0)
+      {
+        if(neigh == 3) univ2[i][j] = 1;
+      }
+      else
+      {
+        if(neigh <= 1 || neigh >= 4) univ2[i][j] = 0;
+        if(neigh == 2 || neigh == 3) univ2[i][j] = 1;
+      }
+    }
+  }
+  for (int i=0; i<WIDTH;i++)
+  {
+    for (int j=0; j<HEIGHT;j++)
+    {
+      univ[i][j]=univ2[i][j];
+    }
+  }
+}
+
 void Display(bool univ[HEIGHT][WIDTH])
 {
   system("clear");
@@ -55,11 +112,15 @@ int main()
       case 0:
         scanf("%c",&button_press);
         if (button_press =='q') return 0;       // Q pour quitter
-        else if (button_press =='a') Display(univ);  // A pour afficher la prochaine génération
+        else if (button_press =='a')
+        {
+          evolve(univ);
+          Display(univ);  // A pour afficher la prochaine génération
+        }
         else button_press ='0';
         break;
       case 1:
-        // procedure d'évolution ici
+        evolve(univ);
         Display(univ);
         printf("Generation numéro %d\n", cpt_generation);
         cpt_generation++;
