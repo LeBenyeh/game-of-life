370c9bedfa299a6a677deb89be6875e773ee4b3f
Cart Benjamin <51527949+LeBenyeh@users.noreply.github.com>
2023-12-13 17:52:33 +0100

    Update life.c
    Ajout d'un mode automatique

--- src/life.c
+++ src/life.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <unistd.h>
 #include "inout.h"
+#define Time 1
 void Display(bool univ[HEIGHT][WIDTH])
 {
   system("clear");
@@ -15,29 +16,59 @@ void Display(bool univ[HEIGHT][WIDTH])
     printf("\n");
   }
 }
-
-int main() {
-
+int mode_chosing()
+{
+  char select_mode;
+  int mode = -1;
+  while(mode == -1)
+  {
+  if(select_mode=='M' ||select_mode=='m') mode = 0;
+  else if (select_mode=='A' ||select_mode=='a') mode = 1;
+  else scanf("%c",&select_mode);
+  }
+  return mode;
+}
+int main()
+{
   //-----------------------------Début création des variables------------------------------//
   char button_press='0';
   bool univ[HEIGHT][WIDTH];
   const char * seed = "../seeds/beacon-10x10.life"; // seed à placer ici
+  int mode;
+  int cpt_generation = 0;
 
   //-----------------------------Fin création des variables------------------------------//
-
-
+  //----------------------------Demande du mode-------------------------//
+  system("clear");
+  printf("(A)uto or (M)anual ?\n");
+  mode = mode_chosing();
+  printf("Mode : %d\n", mode);
   load_seed(seed,univ);
   Display(univ); // Premier affichage
+
 
   //----------------------------Début de la boucle -------------------------------------//
   while(1)
   {
+    switch(mode)
+    {
+      case 0:
+        scanf("%c",&button_press);
+        if (button_press =='q') return 0;       // Q pour quitter
+        else if (button_press =='a') Display(univ);  // A pour afficher la prochaine génération
+        else button_press ='0';
+        break;
+      case 1:
+        // procedure d'évolution ici
+        Display(univ);
+        printf("Generation numéro %d\n", cpt_generation);
+        cpt_generation++;
+        sleep(Time);
+        break;
+    }
 
-    scanf("%c",&button_press);
-    if (button_press =='q') return 0;       // Q pour quitter
-    else if (button_press =='a') Display(univ);  // A pour afficher la prochaine génération
-    else button_press ='0';
   }
+
 
   return 0;
 }
