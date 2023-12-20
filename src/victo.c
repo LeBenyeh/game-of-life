c69af6c266b481213ae03fc5dfb439e6fbc89f6f
VictoDTZ <153749183+VictoDTZ@users.noreply.github.com>
2023-12-20 16:37:57 +0100

    Update victo.c

--- src/victo.c
+++ src/victo.c
@@ -4,10 +4,10 @@ int nmod(int val)
   else return val%HEIGHT;
 }
 
-int finalcount(int lig, int col, bool univ[HEIGHT][WIDTH]
+int count(int lig, int col, bool univ[HEIGHT][WIDTH])
 {
   int Periodic, c=0, i, j;
-  if(Periodic = 1)
+  if(Periodic == 1)
   {
     for(i=-1; i<2;i++)
     {
@@ -34,4 +34,33 @@ int finalcount(int lig, int col, bool univ[HEIGHT][WIDTH]
             c++;
   }}}}}
   return c;
+}
+
+void evolve(bool univ[HEIGHT][WIDTH])
+{
+  bool univ2[HEIGHT][WIDTH]={0};
+  int neigh;
+  for (int i=0; i<HEIGHT; i++)
+  {
+    for (int j=0; j<WIDTH; j++)
+    {
+      neigh = count(i,j,univ);
+      if(univ[i][j] == 0)
+      {
+        if(neigh == 3) univ2[i][j] = 1;
+      }
+      else
+      {
+        if(neigh <= 1 || neigh >= 4) univ2[i][j] = 0;
+        if(neigh == 2 || neigh == 3) univ2[i][j] = 1;
+      }
+    }
+  }
+  for (int i=0; i<WIDTH;i++)
+  {
+    for (int j=0; j<HEIGHT;j++)
+    {
+      univ[i][j]=univ2[i][j];
+    }
+  }
 }
